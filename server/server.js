59254febad9de30d43645e4daef8f32eee74c9a2
spydaspider <spydaspider@gmail.com>
2023-09-04 10:51:17 +0000

    Add react app

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT;
-const server = app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`);
-})
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(
+    app.listen(port,()=>{
+    console.log(`Server has started ${port}`)
+    })).catch((err)=>console.error(err));
